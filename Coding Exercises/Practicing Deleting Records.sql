885be0237625930a838f93810478edb95dd82f9b
Laurent <98193541+laurentlorquet@users.noreply.github.com>
2024-01-05 12:22:26 -0500

    Finished Section 2

---
+++ Coding Exercises/Practicing Deleting Records.sql
@@ -0,0 +1,4 @@
+-- Coding Exercise 7: Practicing Deleting Rows
+-- Author: Laurent Lorquet
+-- 1/05/2024
+
---
+++ Coding Exercises/Updating Records.sql
@@ -0,0 +1,14 @@
+-- Coding Exercise 6: Updating Records in a table
+-- Author: Laurent Lorquet
+-- 1/05/2024
+
+-- Write query here to update the 'units_sold' 
+-- of the phone with name 'N8' to 8543
+UPDATE phones
+SET units_sold = 8543
+WHERE name = 'N8';
+
+
+-- Write query here to select all rows and columns 
+-- of the 'phones' table
+SELECT * FROM phones

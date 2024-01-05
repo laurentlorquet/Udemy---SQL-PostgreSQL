039ecc6c62834414f75831a0f165faf863d91cac
Laurent <98193541+laurentlorquet@users.noreply.github.com>
2024-01-04 20:30:07 -0500

    Exercise 1: Create, Insert, and Select
    -- Write a SELECT statement to retrieve both rows inserted into the movies table.  
    -- Select both the title column and the box_office column.
    -- You don't need to change these lines

---
+++ Create, Insert, and Select.sql
@@ -0,0 +1,16 @@
+-- Write a SELECT statement to retrieve both rows inserted into the movies table.  
+-- Select both the title column and the box_office column.
+-- You don't need to change these lines
+CREATE TABLE movies (
+    title VARCHAR(60),
+    box_office INTEGER
+);
+
+INSERT INTO movies (title, box_office)
+VALUES 
+    ('The Avengers', 1500000000),
+    ('Batman v Superman', 873000000);
+    
+
+-- WRITE YOUR SOLUTION BELOW THIS LINE!
+SELECT title, box_office FROM movies;  

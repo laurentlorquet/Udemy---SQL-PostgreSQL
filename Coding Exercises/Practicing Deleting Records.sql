c91c3aab420273470f7d53c78f9b0ff87ccde15d
Laurent <98193541+laurentlorquet@users.noreply.github.com>
2024-01-05 12:22:57 -0500

    Finished Quizzes for Section 2

--- Coding Exercises/Practicing Deleting Records.sql
+++ Coding Exercises/Practicing Deleting Records.sql
@@ -2,3 +2,16 @@
 -- Author: Laurent Lorquet
 -- 1/05/2024
 
+--We are once again working with the phones table.
+--You are going to write two separate queries
+--The first query should delete all phones that were created by Samsung
+--The second query should select all rows and columns of the phones table
+
+
+
+-- Write your delete SQL here
+DELETE FROM phones
+WHERE manufacturer = 'Samsung';
+
+-- Write query here to select all rows and columns from phones
+SELECT * FROM phones;

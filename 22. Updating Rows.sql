d6b2d289c3cd3db424fe71b5053fafe51c7bac14
Laurent <98193541+laurentlorquet@users.noreply.github.com>
2024-01-04 21:29:43 -0500

    Section 2 - Filtering Records

---
+++ 22. Updating Rows.sql
@@ -0,0 +1,12 @@
+-- 22. Updating Rows
+-- Updating a database
+-- Author: Laurent Lorquet
+-- 1/04/2023
+
+UPDATE cities
+SET population = 39505000
+WHERE name = "Tokyo";
+
+
+--now to see if the row was updated we need a new query
+SELECT * FROM cities;

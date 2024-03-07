05149dfb30023dd2db4795f77ed32458aa238188
Laurent <98193541+laurentlorquet@users.noreply.github.com>
2024-03-06 19:43:46 -0500

    A Gotcha with Count

--- Section 5 - Aggregation of Records/Combining Group By and Aggregates.sql
+++ Section 5 - Aggregation of Records/Combining Group By and Aggregates.sql
@@ -2,7 +2,12 @@
 -- Author: Laurent Lorquet
 -- 3/05/2023
 
-
+-- Users with the largest ID tagged with them
 SELECT user_id, MAX(id)
+FROM comments
+GROUP BY user_id;
+
+-- Counting the number of comments a user has made
+SELECT user_id, COUNT(id) AS num_comments_created
 FROM comments
 GROUP BY user_id;
